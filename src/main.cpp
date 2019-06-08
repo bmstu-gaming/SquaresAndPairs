262c2c6d29c0f9e0e114ccf270ffcfc6d3b0dd86
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2019-06-08 14:57:34 +0300

    add fps counter

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
+#include <fstream>
+
 /*
  *	Start point
  */
@@ -15,19 +17,34 @@ int main() {
 	window.setPosition(sf::Vector2i(500, 50));
 	/*	Doesn't work
 	//	vertical synchronization enable
-	window.setVerticalSyncEnabled(true);
 	*/
+	window.setVerticalSyncEnabled(true);
 	
 	//	given framerate
-	window.setFramerateLimit(300);
+	//window.setFramerateLimit(300);
 	/*
 	sf::CircleShape shape(100.f, 300);
 	shape.setFillColor(sf::Color::Green);
 	*/
 
+	// Load font
+	sf::Font font;
+	if (!font.loadFromFile("assets/Consolas.ttf")) {
+		std::cerr << "Font load error" << std::endl;
+	}
+
+	// Add text for FPS
+	sf::Text fpsText;
+	fpsText.setFont(font);
+	fpsText.setCharacterSize(24);
+	fpsText.setFillColor(sf::Color::White);
+
+	// Add square
 	sf::RectangleShape square(sf::Vector2f(50, 50));
 	square.setPosition(600, 300);
 	square.setFillColor(sf::Color::Red);
+
+	sf::Clock clock;
 
 	//	run the program as long as the window is open
 	while ( window.isOpen() ) {
@@ -45,6 +62,8 @@ int main() {
 			if ( event.type == sf::Event::Resized ) {
 				std::cout << "new width: "	<< event.size.width		<< std::endl;
 				std::cout << "new height: " << event.size.height	<< std::endl;
+
+				window.setSize(sf::Vector2u(event.size.width, event.size.height));
 			}
 			//	LostFocus events
 			if ( event.type == sf::Event::LostFocus ) {
@@ -64,6 +83,7 @@ int main() {
 			if ( event.type == sf::Event::KeyPressed ) {
 				if ( event.key.code == sf::Keyboard::Escape ) {
 					std::cout << "the escape key was pressed" << std::endl;
+					window.close();
 				}
 			}
 			//	The MouseButtonPressed and MouseButtonReleased events
@@ -127,10 +147,14 @@ int main() {
 
 		}
 
+		sf::Time elapsed = clock.restart();
+		fpsText.setString(std::to_string(1.f / elapsed.asSeconds()));
+
+
 		window.clear(sf::Color::Black);
 
 		window.draw(square);
-
+		window.draw(fpsText);
 
 		window.display();
 
